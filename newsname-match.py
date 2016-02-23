9429f369fea54622740ddcd657f1f3161eefa098
Sasan Bahadaran <sasanbahadaran@Sasans-MacBook-Pro.local>
2016-02-23 18:20:12 -0500

    added script file

---
+++ newsname-match.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+#newsname-match.py
+
+#Title:         NewsName Matcher
+#Version:       1.0
+#Author:        Sasan Bahadaran
+#Date:          2/23/16
+#Organization:  District Data Labs
+
+##############################################
+#   IMPORTS
+##############################################
+import os
+
+#list of main candidates
+candidates = ["Woodrow Wilson","Charles E. Hughes"]
+
+path = '/dates/19160101'
+
+for filename in os.listdir(path):
+    print filename
